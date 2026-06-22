3920928869fa5423a9144ec25051df4e0e5bcefd
Lúcio Campos Nunes <luciocnunes@hotmail.com>
2026-06-22 15:53:17 -0300

    Update PCaixaExeletronico.dpr

--- PCaixaExeletronico.dpr
+++ PCaixaExeletronico.dpr
@@ -2,7 +2,6 @@ program PCaixaExeletronico;
 
 uses
   Vcl.Forms,
-<<<<<<< HEAD
   UPrincipal in 'src\view\UPrincipal.pas' {Form6},
   UDmPrincipal in 'src\view\UDmPrincipal.pas' {DMPrincipal: TDataModule},
   UEstoque in 'src\view\UEstoque.pas' {FrmEstoque},
@@ -12,9 +11,6 @@ uses
   USaque in 'src\view\USaque.pas' {frmSaque},
   UCliente in 'src\view\UCliente.pas' {frmCliente},
   UNotificacoes in 'src\view\UNotificacoes.pas' {frmNotificacao};
-=======
-  UPrincipal in 'UPrincipal.pas' {Form6};
->>>>>>> d604cf916c505542d6ed3a6fd8b147b05ba5e265
 
 {$R *.res}
 
@@ -22,13 +18,10 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm6, Form6);
-<<<<<<< HEAD
   Application.CreateForm(TDMPrincipal, DMPrincipal);
   Application.CreateForm(TFrmEstoque, FrmEstoque);
   Application.CreateForm(TfrmSaque, frmSaque);
   Application.CreateForm(TfrmCliente, frmCliente);
   Application.CreateForm(TfrmNotificacao, frmNotificacao);
-=======
->>>>>>> d604cf916c505542d6ed3a6fd8b147b05ba5e265
   Application.Run;
 end.
